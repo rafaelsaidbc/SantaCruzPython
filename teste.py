a1951e9d055998b61e1c8c2e967933bf9575398a
Rafael Said <rafaelsaidbc@yahoo.com.br>
2019-02-15 15:04:08 -0200

    Delete teste.py

--- teste.py
+++
@@ -1,7 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials
-
-cred = credentials.Cert('path/to/serviceKey.json')
-firebase_admin.initialize_app(cred, {
-    'databaseURL' : 'https://my-db.firebaseio.com'
-})
